b3f382a0e7042f424938fd5d3e0d19a59bd4b37f
vic8722 <victoria.trash@gmail.com>
2016-07-09 16:46:14 -0700

    Add tray door. Add tea cup. Add animations for both upon win.

--- js/slot_machine.js
+++ js/slot_machine.js
@@ -4,7 +4,10 @@
 
     $('#slotMachine').html(reelDiv);
     $('#playFancy').on('click',function(){
+        closeDoor();
         $("#winner").removeClass('winner');
+        $("#winner").html('');
+
         $(".led-orange").removeClass('blink-orange');
         $(".led-blue").removeClass('blink-blue');
         console.log("i'm clicked")
@@ -50,184 +53,47 @@
             var message;
             if (endNum[0]===endNum[1] && endNum[0]===endNum[2]) {
                 message = 'Congratulations you won '+ prizes[endNum[0]] + '!!';
-                $("#winner").addClass('winner');
-                $(".led-orange").addClass('blink-orange');
-                $(".led-blue").addClass('blink-blue');
+                prizeWon();
             }
             else {
                 message = 'Rats! Try again?';
             }
             $('#winnerStatus').html(message);
         };
-        // function finalRotation(){
-
-        // }
     });
 
 
-    // var base = $('#slot')
-    // base.setup = function() {
-
-    //         // set sizes
-
-    //         var $list = this;
-
-    //         base.liHeight = 25;
-    //         base.liWidth = 25;
-
-    //         base.liCount = 3;
-
-    //         base.listHeight = base.liHeight * base.liCount;
-
-    //         base.increment = (7000 / 3) / 3;
-
-    //         $list.css('position', 'relative');
-
-    //         base.$wrapper = $list.wrap('<div class="jSlots-wrapper"></div>').parent();
-
-    //         // remove original, so it can be recreated as a Slot
-    //         base.$el.remove();
-
-    //         // clone lists
-    //         for (var i = base.options.number - 1; i >= 0; i--){
-    //             base.allSlots.push( new base.Slot() );
-    //         }
-
-    //     };
-
-    //     base.bindEvents = function() {
-    //         $(base.options.spinner).bind(base.options.spinEvent, function(event) {
-    //             if (!base.isSpinning) {
-    //                 base.playSlots();
-    //             }
-    //         });
-    //     };
-
-    // // Slot contstructor
-    //     base.Slot = function() {
-
-    //         this.spinSpeed = 0;
-    //         this.el = base.$el.clone().appendTo(base.$wrapper)[0];
-    //         this.$el = $(this.el);
-    //         this.loopCount = 0;
-    //         this.number = 0;
-
-    //     };
-
-
-    //     base.Slot.prototype = {
-
-    //         // do one rotation
-    //         spinEm : function() {
-
-    //             var that = this;
-
-    //             that.$el
-    //                 .css( 'top', -base.listHeight )
-    //                 .animate( { 'top' : '0px' }, that.spinSpeed, 'linear', function() {
-    //                     that.lowerSpeed();
-    //                 });
-
-    //         },
-
-    //         lowerSpeed : function() {
-
-    //             this.spinSpeed += base.increment;
-    //             this.loopCount++;
-
-    //             if ( this.loopCount < base.options.loops ) {
-
-    //                 this.spinEm();
-
-    //             } else {
-
-    //                 this.finish();
-
-    //             }
-    //         },
-
-    //         // final rotation
-    //         finish : function() {
-
-    //             var that = this;
-
-    //             var endNum = base.randomRange( 1, base.liCount );
-
-    //             var finalPos = - ( (base.$liHeight * endNum) - base.$liHeight );
-    //             var finalSpeed = ( (this.spinSpeed * 0.5) * (base.liCount) ) / endNum;
-
-    //             that.$el
-    //                 .css( 'top', -base.listHeight )
-    //                 .animate( {'top': finalPos}, finalSpeed, base.options.easing, function() {
-    //                     base.checkWinner(endNum, that);
-    //                 });
-
-    //         }
-
-    //     };
-
-    //     base.checkWinner = function(endNum, slot) {
-
-    //         base.doneCount++;
-    //         // set the slot number to whatever it ended on
-    //         slot.number = endNum;
-
-    //         // if its in the winners array
-    //         if (
-    //             ( $.isArray( base.options.winnerNumber ) && base.options.winnerNumber.indexOf(endNum) > -1 ) ||
-    //             endNum === base.options.winnerNumber
-    //             ) {
-
-    //             // its a winner!
-    //             base.winCount++;
-    //             base.winners.push(slot.$el);
-
-    //         }
-
-    //         if (base.doneCount === base.options.number) {
-
-    //             var finalNumbers = [];
-
-    //             $.each(base.allSlots, function(index, val) {
-    //                 finalNumbers[index] = val.number;
-    //             });
-
-    //             if ( $.isFunction( base.options.onEnd ) ) {
-    //                 base.options.onEnd(finalNumbers);
-    //             }
-
-    //             if ( base.winCount && $.isFunction(base.options.onWin) ) {
-    //                 base.options.onWin(base.winCount, base.winners, finalNumbers);
-    //             }
-    //             base.isSpinning = false;
-    //         }
-    //     };
-
-
-    //     base.playSlots = function() {
-
-    //         base.isSpinning = true;
-    //         base.winCount = 0;
-    //         base.doneCount = 0;
-    //         base.winners = [];
-
-    //         if ( $.isFunction(base.options.onStart) ) {
-    //             base.options.onStart();
-    //         }
-
-    //         $.each(base.allSlots, function(index, val) {
-    //             this.spinSpeed = 0;
-    //             this.loopCount = 0;
-    //             this.spinEm();
-    //         });
-
-    //     };
-
-
-    //     base.onWin = function() {
-    //         if ( $.isFunction(base.options.onWin) ) {
-    //             base.options.onWin();
-    //         }
-    //     };
-
 })(jQuery);
+
+
+var openDoor = function(){
+    $("#tea-img").css({
+        'display': 'flex',
+        'height': '0px'
+    });
+    $("#tray-door").animate( {
+        height: "10px"
+        }, 400, 'linear',prizeTea)
+};
+
+var closeDoor = function(){
+    $("#tray-door").animate( {
+        height: "110px"
+        }, 400, 'linear',function(){
+
+        })
+};
+
+var prizeWon = function(){
+    $("#winner").addClass('winner');
+    $("#winner").html('WINNER!');
+    $(".led-orange").addClass('blink-orange');
+    $(".led-blue").addClass('blink-blue');
+
+    openDoor();
+};
+
+var prizeTea = function(){
+
+    $("#tea-img").animate({height: "50px"},800,'linear')
+};
